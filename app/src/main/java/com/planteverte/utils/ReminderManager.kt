4d5b7e2bf3ff55ca14992a12e86143eb6b08e856
98731 <105843577+teremuhamblin@users.noreply.github.com>
2026-09-13 17:19:24 +0200

    Add ReminderManager with test function for Toast

---
+++ app/src/main/java/com/planteverte/utils/ReminderManager.kt
@@ -0,0 +1,10 @@
+package com.planteverte.utils
+
+import android.content.Context
+import android.widget.Toast
+
+object ReminderManager {
+    fun test(context: Context) {
+        Toast.makeText(context, "Rappel test PlanteVerte", Toast.LENGTH_SHORT).show()
+    }
+}
