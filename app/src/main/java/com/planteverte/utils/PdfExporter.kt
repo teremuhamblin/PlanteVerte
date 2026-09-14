fdb9c027d8ae5c6654e45fbb21188d66548c8b3d
98731 <105843577+teremuhamblin@users.noreply.github.com>
2026-09-14 08:33:24 +0200

    Add PdfExporter class for PDF export functionality
    Initial implementation of PdfExporter class with a method to export journal to PDF.

---
+++ app/src/main/java/com/planteverte/utils/PdfExporter.kt
@@ -0,0 +1,12 @@
+package com.planteverte.utils
+
+import android.content.Context
+import java.io.File
+
+class PdfExporter(private val context: Context) {
+
+    fun exportJournalToPdf(): File? {
+        // TODO: générer un PDF dans assets/pdf ou storage
+        return null
+    }
+}
